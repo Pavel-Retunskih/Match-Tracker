ea4441e89323a1d334f972a9e3b206362012ba7e
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-03-07 02:42:58 +0300

    fix

--- src/entities/player/ui/Player.tsx
+++ src/entities/player/ui/Player.tsx
@@ -5,9 +5,9 @@ import avatar_global from '../../../assets/avatar_global.png'
 export function Player({username, kills}: PlayerType) {
 
   return (
-      <div className={'w-[286px]  px-6 py-2 flex justify-between items-center bg-[#101318]'}>
-        <div className={'flex justify-between items-center'}>
-          <div className={'flex items-center gap-2'}>
+      <div className={'flex-wrap gap-3 px-6 py-2 flex justify-around items-center bg-[#101318]'}>
+        <div className={'flex justify-between  items-center'}>
+          <div className={'flex items-center gap-2 '}>
             <img src={avatar_global} alt="Изображение игрока"/>
             <span>{username}</span></div>
         </div>
