2b9a4ac47e45ba708d9da3e586120e9350865d9e
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-03-14 23:55:51 +0300

    fix

--- src/entities/team/ui/TeamInfo.tsx
+++ src/entities/team/ui/TeamInfo.tsx
@@ -1,20 +1,16 @@
 import {Player} from "../../player/ui/Player.tsx";
 import {Team} from "../model/type.ts";
+import {StatCard} from "../../../shared/statCard/statCard.tsx";
 
 export function TeamInfo({points, players, place, total_kills}: Team) {
-  return (<div className={'flex flex-col gap-2 mt-11'}>
+  return (<div className={'flex flex-col gap-2 flex-grow'}>
     <div className={'flex w-full justify-between items-center gap-2'}>
       {players.map(({username, kills}) => <Player key={username} kills={kills} username={username}/>)}
     </div>
-    <div className={'flex justify-around bg-[#101318] py-[12px] px-[24px]'}>
-      <div className={'text-s gap-2 flex'}><span className={'text-[#FAFAFA66]'}>Points:</span>
-        <span>{points}</span>
-      </div>
-      <div className={'text-s gap-2 flex'}><span className={'text-[#FAFAFA66]'}>Место:</span>
-        <span>{place}</span>
-      </div>
-      <div className={'text-s gap-2 flex'}><span className={'text-[#FAFAFA66]'}>Всего
-        убийств:</span> <span>{total_kills}</span></div>
+    <div className={'flex justify-between bg-[#101318] py-[14px] px-3 rounded'}>
+      <StatCard title={'Points'} value={points}/>
+      <StatCard title={'Место:'} value={place}/>
+      <StatCard title={'Всего убийств:'} value={total_kills}/>
     </div>
   </div>)
 }
