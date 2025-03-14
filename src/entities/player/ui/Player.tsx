6d5bd5eb8e8dd139b0239e64c84938aa44b927be
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-03-14 23:55:37 +0300

    refactor

--- src/entities/player/ui/Player.tsx
+++ src/entities/player/ui/Player.tsx
@@ -1,20 +1,18 @@
 import {PlayerType} from "../model/type.ts";
 import avatar_global from '../../../assets/avatar_global.png'
+import {StatCard} from "../../../shared/statCard/statCard.tsx";
 
 
 export function Player({username, kills}: PlayerType) {
 
   return (
-      <div className={'flex-wrap gap-[6px] px-3 py-2 flex justify-around items-center bg-[#101318]'}>
+      <div className={'flex-wrap gap-[6px] flex-grow py-2 flex justify-around items-center rounded bg-[#101318]'}>
         <div className={'flex justify-between  items-center'}>
           <div className={'flex items-center gap-2 '}>
             <img src={avatar_global} className={'h-8 w-8 md:h-9 md:w-9'} alt="Изображение игрока"/>
             <span>{username}</span></div>
         </div>
-        <div className={'flex items-center gap-2'}>
-          <span className={'text-s text-[#FAFAFA66]'}>Убийств:</span>
-          <span>{kills}</span>
-        </div>
+        <StatCard title={'Убийств:'} value={kills}/>
       </div>
   )
 }
