2bc750a217056f5e5f54c1cc24887ddd1b2d3e87
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-03-14 18:33:13 +0300

    fix width and height team icon

--- src/entities/match/ui/MatchInfo/TeamInfo/TeamInfo.tsx
+++ src/entities/match/ui/MatchInfo/TeamInfo/TeamInfo.tsx
@@ -5,7 +5,7 @@ import {memo} from "react";
 export const TeamInfo = memo(({teamName, isReverse}: { teamName: string, isReverse?: boolean }) => {
 
   return <div className={`flex items-center gap-4 ${isReverse ? 'flex-row-reverse' : ''}`}>
-    <img className={'h-12 w-12'} src={team_icon} alt="Иконка команды"/>
+    <img className={'h-7 w-7 md:h-12 md:w-12'} src={team_icon} alt="Иконка команды"/>
     <span>{teamName}</span>
   </div>
 })
